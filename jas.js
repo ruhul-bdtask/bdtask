c3f53e68900989083250a8e9482772d1e0886b18
ruhul-bdtask <ruhul@bdtask.net>
2024-03-24 17:30:30 +0600

    m2

---
+++ jas.js
@@ -0,0 +1,11 @@
+const text = 'this is text'
+const text2 = new String('this is text')
+const ar = text.split()
+
+console.log( text);
+// console.log( text2);
+console.log( ar);
+
+
+// this is committed from new branch
+// m2
